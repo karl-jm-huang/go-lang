5fd722ac412d89e6e6dc8d47de213c0406589720
karlHJM <1259040327@qq.com>
2017-11-14 23:16:40 +0800

    Update service.go

--- cloudgo/service/service.go
+++ cloudgo/service/service.go
@@ -5,11 +5,14 @@ import (
 )
 
 func NewServer(port string) {
+	//创建一个典型的martini实例
 	mt := martini.Classic()
-
+	
+	//接收对/login的GET方法请求，第二个参数是对该请求的处理方法，获取其参数usr的值并打印
 	mt.Get("/login/:usr", func(params martini.Params) string {
 		return "user " + params["usr"] + " has logged in.\n"
 	})
-
+	
+	//运行该服务器
 	mt.RunOnAddr(":" + port)
 }
